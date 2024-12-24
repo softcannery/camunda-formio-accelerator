1193f478cd12601ea92f7c703902cac545c8864e
Volodymyr Kotyk <harley4ik@gmail.com>
2024-12-24 14:50:12 -0800

    prettier fix

--- cucumber-tests/src/test/java/cucumber/pages/CamundaModelerAppPage.java
+++ cucumber-tests/src/test/java/cucumber/pages/CamundaModelerAppPage.java
@@ -84,9 +84,9 @@ public class CamundaModelerAppPage {
         opt.setBinary(camundaModelerPath);
         opt.setBrowserVersion(browserVersion);
         opt.addArguments("--user-data-dir=" + userDirPath);
-//        opt.addArguments("--disable-dev-shm-usage");
-//        opt.setExperimentalOption("useAutomationExtension", false);
-//        opt.addArguments("--no-sandbox");
+        //        opt.addArguments("--disable-dev-shm-usage");
+        //        opt.setExperimentalOption("useAutomationExtension", false);
+        //        opt.addArguments("--no-sandbox");
         if (headless) {
             opt.addArguments("--headless=new");
         }
