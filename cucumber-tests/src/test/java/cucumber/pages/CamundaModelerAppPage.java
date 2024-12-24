525cc5c83bd96de5eb17481f3288f776730e558a
Mikhail Vasylchenko <mikhail.vasylchenko@introproventures.com>
2024-12-24 12:41:47 -0800

    add additional arguments to chromedriver

--- cucumber-tests/src/test/java/cucumber/pages/CamundaModelerAppPage.java
+++ cucumber-tests/src/test/java/cucumber/pages/CamundaModelerAppPage.java
@@ -86,6 +86,10 @@ public class CamundaModelerAppPage {
         opt.setBinary(camundaModelerPath);
         opt.setBrowserVersion(browserVersion);
         opt.addArguments("--user-data-dir=" + userDirPath);
+        opt.addArguments("--no-sandbox");
+        opt.addArguments("disable-infobars");
+        opt.addArguments("--disable-extensions");
+        opt.addArguments("--disable-dev-shm-usage");
         if (headless) {
             opt.addArguments("--headless=new");
         }
