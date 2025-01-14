31e7a890d0d07b17e800ee8295f6ad5ddf635120
mike-vasylchenko <55990475+mike-vasylchenko@users.noreply.github.com>
2025-01-13 17:50:35 -0800

    remove ports that are not used anymore from docker-compose.yml (#187)
    Signed-off-by: mike-vasylchenko <55990475+mike-vasylchenko@users.noreply.github.com>

--- cucumber-tests/src/test/java/cucumber/api/Base.java
+++ cucumber-tests/src/test/java/cucumber/api/Base.java
@@ -2,9 +2,9 @@ package cucumber.api;
 
 public class Base {
 
-    public static String camundaUrl = "http://localhost/bpm";
+    public static String camundaUrl = "http://localhost:80/bpm";
     public static String username = "kermit";
     public static String password = "password";
-    public static String fileuploadUrl = "http://localhost:8081";
+    public static String fileuploadUrl = "http://localhost:80";
     public static String mailApiUrl = "http://localhost:8088/";
 }
